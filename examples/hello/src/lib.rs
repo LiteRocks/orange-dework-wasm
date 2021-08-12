8829723d8fafaa620ab6876f3e1f5a272b79f1ec
zhoupw@gmail.com <zhoupw@gmail.com>
2021-08-12 10:51:31 +0800

    update code

--- examples/hello/src/lib.rs
+++ examples/hello/src/lib.rs
@@ -31,12 +31,33 @@ pub struct ScoreResult {
 pub fn invoke() {
     oscore::set_panic_handler();
     let input = runtime::input();
-    let p: AssetInfoData = serde_json::from_slice(input.as_slice()).expect("hhhh");
+    let p: AssetInfoData = serde_json::from_slice(input.as_slice()).expect("invalid param");
 
-    let len = p.asset_infos.len();
+//    let len = p.asset_infos.len();
+    let mut worthy = 0.0;
+    p.asset_infos.iter().for_each(|asset|{
+        let price = asset.price.parse::<f64>().unwrap();
+        let amount = asset.xday_sum.amount.parse::<f64>().unwrap();
+        worthy = worthy + amount * price / asset.xday_sum.days as f64
+    });
 
-    let score = ScoreResult { score: len as u32};
+
+
+    let score = ScoreResult { score: calcScore(worthy)};
 
     let result = serde_json::to_string(&score).unwrap();
     runtime::ret(result.as_ref());
+}
+
+fn calcScore(worthy:f64)->u32 {
+    let w = worthy as u32;
+    if  w < 100 {
+        100
+    }else if w < 1000{
+        300
+    }else if w < 5000{
+        500
+    }else{
+        800
+    }
 }
